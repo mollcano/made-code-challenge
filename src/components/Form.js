148f88b0b7c3852408c6a5f864f6773db4a66584
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-13 17:26:23 -0600

    form styled for mobile

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,19 @@ export default class Form extends Component {
   render(){
     return(
       <div className="form">
-        <h1>form here</h1>
+        <form>
+          <label for="name">First Name</label>
+          <input type="text" name="name" value="Jane" />
+          <label for="job">Job Title</label>
+          <input type="text" name="job" value="Developer" />
+          <label for="birthday">Birthday</label>
+          <input type="text" name="birthday" value="mm/dd/yyyy" />
+          <label for="bio">Bio</label>
+          <textarea name="bio" rows="8" cols="10">A few words about yourself</textarea>
+        </form>
+        <div className="submit">
+          <img className="submitButton" src="./images/img-save.png" alt="save" />
+        </div>
       </div>
     )
   }
