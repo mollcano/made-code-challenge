70fff916fdce5726a596c6ad35dd8945305ab856
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-13 19:49:26 -0600

    have nav changing responsively

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,17 +5,19 @@ export default class Form extends Component {
   render(){
     return(
       <div className="form">
-        <form>
-          <label for="name">First Name</label>
-          <input type="text" name="name" value="Jane" />
-          <label for="job">Job Title</label>
-          <input type="text" name="job" value="Developer" />
-          <label for="birthday">Birthday</label>
-          <input type="text" name="birthday" value="mm/dd/yyyy" />
-          <label for="bio">Bio</label>
-          <textarea name="bio" rows="8" cols="10">A few words about yourself</textarea>
-        </form>
-        <div className="submit">
+        <div className="formContainer">
+          <form>
+            <label htmlFor="name">First Name</label>
+            <input type="text" name="name" defaultValue="Jane" />
+            <label htmlFor="job">Job Title</label>
+            <input type="text" name="job" defaultValue="Developer" />
+            <label htmlFor="birthday">Birthday</label>
+            <input type="text" name="birthday" defaultValue="mm/dd/yyyy" />
+            <label htmlFor="bio">Bio</label>
+            <textarea name="bio" rows="8" cols="10" defaultValue="A few words about yourself"></textarea>
+          </form>
+        </div>
+        <div className="submitContainer">
           <img className="submitButton" src="./images/img-save.png" alt="save" />
         </div>
       </div>
