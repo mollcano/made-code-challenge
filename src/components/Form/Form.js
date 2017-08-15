e52112d81ce7724e862fbc5a5cdc5ce012cb5039
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-15 10:57:31 -0600

    have save button in right place

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,7 +16,9 @@ export default class Form extends Component {
             <input type="text" name="birthday" defaultValue="mm/dd/yyyy" />
             <label htmlFor="bio">Bio</label>
             <textarea name="bio" rows="8" cols="10" defaultValue="A few words about yourself"></textarea>
-            <img className="submitButton" src={require("./images/img-save.png")} alt="save" />
+            <div className="submitContainer">
+              <img className="submitButton" src={require("./images/img-save.png")} alt="save" />
+            </div>
           </form>
         </div>
       </div>
